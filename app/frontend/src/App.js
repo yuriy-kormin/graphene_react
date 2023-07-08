d0744f5c96610f38af31810f4ae66fba1981a9b7
= <=>
2023-07-08 20:51:44 +0400

    add user useState()

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 function App() {
+  const [user, setUser] = useState({is_login:false})
+
   return (
     <div className="App">
       <Navbar collapseOnSelect expand="md" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
@@ -15,7 +17,7 @@ function App() {
                 <Nav className="me-auto">
                 </Nav>
                 <Nav>
-                    <Nav.Item className={"text-light"}>username</Nav.Item>
+                    <Nav.Item className={"text-light"}>{user.is_login?user.username:""}</Nav.Item>
                     <Nav.Link href={"logout/"}>logout</Nav.Link>
                 </Nav>
             </Navbar.Collapse>i
