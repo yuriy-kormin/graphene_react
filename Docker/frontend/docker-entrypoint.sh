31e88a6f65c8ad975121bb2b38430b9d46ce76c3
= <=>
2023-08-14 17:11:31 +0400

    fix allowed hosts

---
+++ Docker/frontend/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -n "$CERTBOT_DOMAINS" ]; then
+  DOMAIN=$(echo "$CERTBOT_DOMAINS" | cut -d',' -f1)
+  REACT_APP_BACKEND_URL="http://${DOMAIN}/graphql/"
+  echo "Setting REACT_APP_BACKEND_URL to $REACT_APP_BACKEND_URL"
+  export REACT_APP_BACKEND_URL
+else
+  echo "CERTBOT_DOMAINS environment variable is not set. REACT_APP_BACKEND_URL will use default."
+fi
+
+cd /app/
+
+echo "npm run build"
+npm run build
