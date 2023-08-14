724aa69105e9399c1045da3ec6aa2c2ade9ea820
= <=>
2023-08-14 17:16:55 +0400

    fix allowed hosts

--- Docker/frontend/docker-entrypoint.sh
+++ Docker/frontend/docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -n "$CERTBOT_DOMAINS" ]; then
-  DOMAIN=$(echo "$CERTBOT_DOMAINS" | cut -d',' -f1)
+if [ -n "$ALLOWED_HOSTS" ]; then
+  DOMAIN=$(echo "$ALLOWED_HOSTS" | cut -d',' -f1)
   REACT_APP_BACKEND_URL="http://${DOMAIN}/graphql/"
   echo "Setting REACT_APP_BACKEND_URL to $REACT_APP_BACKEND_URL"
   export REACT_APP_BACKEND_URL
 else
-  echo "CERTBOT_DOMAINS environment variable is not set. REACT_APP_BACKEND_URL will use default."
+  echo "ALLOWED_HOSTS environment variable is not set"
 fi
 
 cd /app/
